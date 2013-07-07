16d6c2514ab077614995e8316bbc678c4dd3ccde
D Tucker <danielle@tuckerlabs.com>
2013-07-06 22:30:46 -0700

    Refactor tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,29 +2,52 @@ require 'spec_helper'
 
 describe "Static pages" do
   subject { page }  
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: name) }
+  end
+
   describe "Home Page" do
     before { visit root_path }
-    it { should have_selector('h1', text: 'Sample App')}
-    it { should have_selector('title', 
-                              text: "Ruby on Rails Tutorial Sample App")}
+    let(:heading) {'Sample App'}
+    let(:name) { ''}
+
+    it_should_behave_like "all static pages"
     it {should_not have_selector('title', text: "| Home")}
   end
 
   describe "Help Page" do
     before { visit help_path }
-    it { should have_selector('h1', text: 'Help')}
-    it { should have_selector('title',  text: "| Help")}
+    let(:heading) { 'Help'}
+    let(:name) {'| Help'}
+    it_should_behave_like "all static pages"
   end
 
   describe "About Page" do
     before { visit about_path }
-    it { should have_selector('h1', text: 'About Us') }
-    it { should have_selector('title', text: "| About Us") }
+    let(:heading) { 'About Us'}
+    let(:name) {'| About Us'}
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
-    it { should have_selector('h1', text: 'Contact')}
-    it { should have_selector('title', text: "| Contact")}
-  end
+    let(:heading) { 'Contact'}
+    let(:name) {'| Contact'}
+    it_should_behave_like "all static pages"
+ end
+
+ it "should have the right links on the layout" do
+     visit root_path
+     click_link "About"
+     page.should have_selector 'title', text: 'About Us'
+     click_link "Help"
+     page.should have_selector 'title', text: 'Help'
+     click_link "Home"
+     click_link "Sign up now!"
+     page.should have_selector 'title', text: 'Sign up'
+     click_link "sample app"
+     page.should have_selector 'h1', text: 'Welcome'
+ end
 end
